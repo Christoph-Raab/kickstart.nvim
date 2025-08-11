f7ad5636a686a122a7bdb07d6eda1837cbfe004d
Christoph Raab <58324151+Christoph-Raab@users.noreply.github.com>
2025-08-11 08:16:23 +0000

    cr: enforce 'osc52' for ssh copy/paste

--- init.lua
+++ init.lua
@@ -116,6 +116,18 @@ vim.o.showmode = false
 --  See `:help 'clipboard'`
 vim.schedule(function()
   vim.o.clipboard = 'unnamedplus'
+  local osc52 = require 'vim.ui.clipboard.osc52'
+  vim.g.clipboard = {
+    name = 'OSC 52',
+    copy = {
+      ['+'] = osc52.copy '+',
+      ['*'] = osc52.copy '*',
+    },
+    paste = {
+      ['+'] = osc52.paste '+',
+      ['*'] = osc52.paste '*',
+    },
+  }
 end)
 
 -- Enable break indent
