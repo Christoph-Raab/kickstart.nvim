c1e2d801fd48c7034d6791aea07ca2a9ab374ee5
Christoph Raab <58324151+Christoph-Raab@users.noreply.github.com>
2025-09-05 12:44:52 +0200

    cr: add custom keymap as custom plugin

---
+++ lua/custom/plugins/keymap.lua
@@ -0,0 +1,7 @@
+local M = {}
+
+local map = vim.keymap.set
+map('n', '<leader>w', '<cmd>write<cr>', { desc = '[W]rite buffer', silent = true })
+map('n', '<leader>z', '<cmd>bdelete<cr>', { desc = 'Close buffer', silent = true })
+
+return M
