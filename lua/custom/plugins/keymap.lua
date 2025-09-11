672f18969cd53c7886898a30f3f287f4fb28f9a2
Christoph Raab <58324151+Christoph-Raab@users.noreply.github.com>
2025-09-11 17:00:23 +0200

    cr: add keymap for maximize/minimize window

--- lua/custom/plugins/keymap.lua
+++ lua/custom/plugins/keymap.lua
@@ -4,5 +4,15 @@ local map = vim.keymap.set
 map('n', '<leader>w', '<cmd>write<cr>', { desc = '[W]rite buffer', silent = true })
 map('n', '<leader>z', '<cmd>bdelete<cr>', { desc = 'Close buffer', silent = true })
 map('n', '<leader>vv', '<c-v>', { desc = 'Visual Block Mode', silent = true, noremap = true })
+map({ 'n', 'i' }, '<C-t>', function()
+  local tabcount = vim.fn.tabpagenr '$'
+  if tabcount == 1 then
+    vim.cmd 'tab split' -- create the extra tab
+  elseif tabcount == 2 then
+    vim.cmd 'tabclose' -- close the extra tab
+  else
+    vim.notify('Multiple tabs open, not closing automatically', vim.log.levels.WARN)
+  end
+end, { noremap = true, desc = '[f]ullscreen current buffer' })
 
 return M
