67f5a1207bea0c67f30f58e268936a38932cdd15
Christoph Raab <58324151+Christoph-Raab@users.noreply.github.com>
2025-09-25 11:25:31 +0200

    cr: remove block select keybinding

--- lua/custom/plugins/keymap.lua
+++ lua/custom/plugins/keymap.lua
@@ -3,7 +3,6 @@ local M = {}
 local map = vim.keymap.set
 map('n', '<leader>w', '<cmd>write<cr>', { desc = '[W]rite buffer', silent = true })
 map('n', '<leader>z', '<cmd>bdelete<cr>', { desc = 'Close buffer', silent = true })
-map('n', '<leader>vv', '<c-v>', { desc = 'Visual Block Mode', silent = true, noremap = true })
 map({ 'n', 'i' }, '<C-t>', function()
   local tabcount = vim.fn.tabpagenr '$'
   if tabcount == 1 then
