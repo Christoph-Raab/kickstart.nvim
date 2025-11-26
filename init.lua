1a117bd2da58f1d3298faca9edd9b2efedfaceba
Christoph Raab <58324151+Christoph-Raab@users.noreply.github.com>
2025-11-26 15:35:47 +0100

    cr: add python-lsp

--- init.lua
+++ init.lua
@@ -711,6 +711,8 @@ require('lazy').setup({
         -- ts_ls = {},
         --
 
+        bashls = {},
+        helm_ls = {},
         lua_ls = {
           -- cmd = { ... },
           -- filetypes = { ... },
@@ -725,10 +727,9 @@ require('lazy').setup({
             },
           },
         },
-        bashls = {},
+        pylsp = {},
         terraformls = {},
         tflint = {},
-        helm_ls = {},
         yamlls = {
           settings = {
             yaml = {
@@ -988,7 +989,7 @@ require('lazy').setup({
     main = 'nvim-treesitter.configs', -- Sets main module to use for opts
     -- [[ Configure Treesitter ]] See `:help nvim-treesitter`
     opts = {
-      ensure_installed = { 'bash', 'c', 'diff', 'html', 'lua', 'luadoc', 'markdown', 'markdown_inline', 'query', 'vim', 'vimdoc' },
+      ensure_installed = { 'bash', 'c', 'diff', 'html', 'lua', 'luadoc', 'markdown', 'markdown_inline', 'python', 'query', 'vim', 'vimdoc' },
       -- Autoinstall languages that are not installed
       auto_install = true,
       highlight = {
