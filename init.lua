9789608c1427a107045df8b6d53462687cfa9dd7
Christoph Raab <58324151+Christoph-Raab@users.noreply.github.com>
2026-02-12 13:58:13 +0100

    cr: add github copilot status icon

--- init.lua
+++ init.lua
@@ -853,6 +853,13 @@ require('lazy').setup({
       -- set use_icons to true if you have a Nerd Font
       statusline.setup { use_icons = vim.g.have_nerd_font }
 
+      local function copilot_component()
+        if vim.g.copilot_enabled then return (vim.g.have_nerd_font and ' ' or 'CP ') end
+        return (vim.g.have_nerd_font and '󰚩 ' or 'cp ')
+      end
+      local orig_fileinfo = statusline.section_fileinfo
+      statusline.section_fileinfo = function(...) return copilot_component() .. orig_fileinfo(...) end
+
       -- You can configure sections in the statusline by overriding their
       -- default behavior. For example, here we set the section for
       -- cursor location to LINE:COLUMN
