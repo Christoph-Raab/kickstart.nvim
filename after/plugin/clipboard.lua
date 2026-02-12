1280bfa9b44079b564d410ef21135f6db177e39f
Christoph Raab <58324151+Christoph-Raab@users.noreply.github.com>
2026-02-12 13:51:46 +0100

    cr: disable clipboard, use custom config

---
+++ after/plugin/clipboard.lua
@@ -0,0 +1,23 @@
+vim.schedule(function()
+  vim.o.clipboard = 'unnamedplus'
+  local osc52 = require 'vim.ui.clipboard.osc52'
+
+  local function paste()
+    return {
+      vim.fn.split(vim.fn.getreg '', '\n'),
+      vim.fn.getregtype '',
+    }
+  end
+
+  vim.g.clipboard = {
+    name = 'OSC 52',
+    copy = {
+      ['+'] = osc52.copy '+',
+      ['*'] = osc52.copy '*',
+    },
+    paste = {
+      ['+'] = paste,
+      ['*'] = paste,
+    },
+  }
+end)
