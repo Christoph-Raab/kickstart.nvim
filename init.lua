0f4f0d5789936274e518660f8faa31d27cf36284
Christoph Raab <58324151+Christoph-Raab@users.noreply.github.com>
2026-02-12 14:01:08 +0100

    cr: add bash ls

--- init.lua
+++ init.lua
@@ -604,6 +604,7 @@ require('lazy').setup({
         --
         -- But for many setups, the LSP (`ts_ls`) will work just fine
         -- ts_ls = {},
+        bashls = {},
         lua_ls = {
           -- cmd = { ... },
           -- filetypes = { ... },
